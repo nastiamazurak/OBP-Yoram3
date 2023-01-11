917101b842206903f3b96455c4c858dd521361b2
berkanarik <75612421+berkanarik@users.noreply.github.com>
2023-01-11 13:35:16 +0100

    Add files via upload

--- Python/parameters.py
+++ Python/parameters.py
@@ -9,21 +9,38 @@ Created on Wed Jan 11 11:10:34 2023
 # This file imports parameters from the CSV file
 # =============================================================================
 
-import csv
+import pandas as pd
 
 try: 
     #read CSV
-    file = open('hhc_job_data.csv')
-    csvreader = csv.reader(file)
-    header = []
-    header = next(csvreader)
-    print(header)
-    rows = []
-    for row in csvreader:
-            rows.append(row)
-    print(rows)
-except:    
-    """
-    data = excel.iloc[:, 0:]
+    data= pd.read_csv('hhc_job_data.csv')
+    
+    #read headers
+    header_names=[]
+    for header in data:
+        header_names.append(header)
+        
+    #convert data to arrays
     data = data.values.tolist()
-    """
+except:    
+    raise ValueError('Wrong file name!')
+
+# create dictionary with job id
+jobs = {}
+for row in data:
+    jobs[row[0]]={}
+    # assign parameters to the job
+    for col in row[1:]:
+        jobs[row[0]][header_names[row.index(col)]] = col
+    jobs[row[0]]['duration']/=60
+
+
+print(jobs)
+
+        
+
+
+"""
+for row in data.client_id:
+    jobs[row]['client_id']=
+"""
