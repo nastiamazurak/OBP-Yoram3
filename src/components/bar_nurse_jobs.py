749fab8b4e0baa26691f03b844a5817b35b7b905
Anastasiia Mazurak <nastia.mazurak@gmail.com>
2023-02-03 12:43:13 +0100

    updated bar_nurse_jobs

--- src/components/bar_nurse_jobs.py
+++ src/components/bar_nurse_jobs.py
@@ -23,25 +23,18 @@ def render(app: Dash) -> html.Div:
             data = jsonpickle.decode(stored_data)
             sol = data["sol"]
             nurses = data["nurses"]
+            schedule = data["schedule"]
             print("Not none!!!!")
-            dff = hp.get_nurse_jobs(sol, nurses)
+            dff = hp.get_nurse_shifts(sol, nurses, schedule)
+            dff2 = hp.get_nurse_jobs(sol, nurses)
 
             if option_slctd == "Weekly overview":
-                weekly = dff.groupby(["client_id"])["Nurses"].nunique()
-                weekly = weekly.reset_index()
-                weekly = pd.DataFrame(weekly).reset_index()
+                weekly = dff.groupby(["client_id"])["nurse id"].nunique().reset_index()
 
-                fig = px.bar(weekly, x="client_id", y="Nurses")
-                fig.update_layout(
-                    autosize=False,
-                    width=1000,
-                    height=500,
-                    title="Number of unique health professionals per client",
-                    yaxis_title="Health professionals",
-                    xaxis_title="Client id",
-                )
-
-                mean_val = round(weekly["Nurses"].mean(), 2)
+                weekly["client_id"] = weekly["client_id"].astype(str)
+                fig = px.bar(weekly, x="client_id", y="nurse id")
+                fig.update_layout(autosize=False, width=1000, height=500)
+                mean_val = round(weekly["nurse id"].mean(), 2)
                 fig.add_annotation(
                     text=f"Mean nr of unique nurses: {mean_val}",
                     showarrow=False,
@@ -49,23 +42,19 @@ def render(app: Dash) -> html.Div:
                     x=-1,
                     y=-0.12,
                 )
+                fig.update_layout(xaxis=dict(tickmode="linear"))
 
                 return html.Div(dcc.Graph(figure=fig), id="my_first_barchart")
 
             else:
-                dff["day"] = dff["day"].str.rstrip(")")
-                dff = dff[dff["day"] == option_slctd]
-                fig = px.bar(dff, x="client_id", y="Nurses")
-                fig.update_layout(
-                    autosize=False,
-                    width=1000,
-                    height=500,
-                    title="Number of unique health professionals per client",
-                    yaxis_title="Health professionals",
-                    xaxis_title="Client id",
-                )
+                dff2["day"] = dff2["day"].str.rstrip(")")
+                dff2["client_id"] = dff2["client_id"].astype(str)
+                dff2 = dff2[dff2["day"] == option_slctd]
+                fig = px.bar(dff2, x="client_id", y="Nurses")
+                fig.update_layout(autosize=False, width=1000, height=500)
+                fig.update_layout(xaxis=dict(tickmode="linear"))
 
-                mean_value = round(dff["Nurses"].mean(), 2)
+                mean_value = round(dff2["Nurses"].mean(), 2)
                 fig.add_annotation(
                     text=f"Mean nr of unique nurses: {mean_value}",
                     showarrow=False,
@@ -73,7 +62,6 @@ def render(app: Dash) -> html.Div:
                     x=-1,
                     y=-0.12,
                 )
-
                 return html.Div(dcc.Graph(figure=fig), id="my_first_barchart")
 
     return html.Div(id="my_first_barchart")
